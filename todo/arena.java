bf37a83784da03a7202b8e95b72475489f919668
RyanG300 <162933580+RyanG300@users.noreply.github.com>
2024-09-14 15:00:15 -0600

    relleno
    tal

---
+++ todo/arena.java
@@ -0,0 +1,11 @@
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class arena {
+    public Dictionary<String, Integer>[][] matriz;
+
+    arena(int x,int y){
+        this.matriz=new Dictionary<>[x][y]; //Pares para evitar problemas xdddd
+    }
+}
